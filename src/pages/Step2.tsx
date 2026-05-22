cc1db0f9a2a7b22eaccf6dab5de91f86d558ff31
haney6438 <haney5438@gamil.com>
2026-05-22 23:38:00 +0900

    feat:정보입력 스텝 레이아웃 구현

--- src/pages/Step2.tsx
+++ src/pages/Step2.tsx
@@ -9,7 +9,29 @@ function Step2() {
         <>
             <Header />
             <Indicator />
-            <button onClick={() => navigate('/step3')}>다음</button>
+            <div className="container">
+                <div className="content">
+                    <p style={{ fontSize: '32px', fontWeight: 'bold' }}>Figma 8주 완성</p>
+                </div>
+                <div className="content">
+                    <p style={{ fontSize: '32px' }}>신청자</p>
+                    <div className="input-section"></div>
+                    <div className="input-section"></div>
+                    <div className="input-section"></div>
+                    <div className="input-section"></div>
+                </div>
+                <div className="content">
+                    <p style={{ fontSize: '32px' }}>단체 정보</p>
+                    <div className="input-section"></div>
+                    <div className="input-section"></div>
+                    <div className="input-section"></div>
+                    <div className="input-section"></div>
+                </div>
+                    <div className="btn2-section">
+                        <button onClick={() => navigate('/step1')}>이전</button>
+                        <button onClick={() => navigate('/step3')}>다음</button>
+                    </div>
+            </div>
         </>
     );
 }
