194fdbf67d7cb18d7492e2cd66d9190d0de2ff51
haney6438 <haney5438@gamil.com>
2026-05-23 14:29:24 +0900

    feat:확인 스텝 UI 구현

--- src/pages/Step3.tsx
+++ src/pages/Step3.tsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 import './Step3.css';
 import Header from "../components/Header";
 import Indicator from "../components/Indicator";
 // import Terms from "../components/Terms";
 
+import { IoIosArrowForward } from "react-icons/io";
 import img1 from "../img/img1.png";
 
 function Step3() {
+    const navigate=useNavigate();
+
     return (
         <>
             <Header />
@@ -15,6 +18,9 @@ function Step3() {
             <div className="container">
                 <div className="content">
                     <div className="check-section1">
+                        <div className="edit" onClick={()=>navigate('/step1')}>
+                            수정
+                        </div>
                         <div className="img-wrap">
                             <div className="lec-remain">
                                 🔥마감임박
@@ -36,19 +42,34 @@ function Step3() {
                     </div>
 
                     <div className="check-section2">
+                        <div className="edit" onClick={()=>navigate('/step2')}>
+                            수정
+                        </div>
                         <div className="indi-wrap">
-
+                            <p style={{ fontSize: '20px', fontWeight: 'bold' }}>신청자</p>
+                            <p style={{ fontSize: '14px' }}>이름: </p>
+                            <p style={{ fontSize: '14px' }}>이메일:</p>
+                            <p style={{ fontSize: '14px' }}>전화번호: </p>
+                            <p style={{ fontSize: '14px' }}>수강동기:</p>
                         </div>
                         <div className="group-wrap">
-
+                            <p style={{ fontSize: '20px', fontWeight: 'bold' }}>단체</p>
+                            <p style={{ fontSize: '14px' }}>단체명: </p>
+                            <p style={{ fontSize: '14px' }}>신청 인원:</p>
+                            <p style={{ fontSize: '14px' }}>참가자 명단: </p>
                         </div>
                     </div>
                 </div>
-                <div className="content">
-
+                <div className="terms-section">
+                    <div className="terms">
+                        <p style={{ fontSize: '14px' }}>이용약관(필수)</p>
+                        <IoIosArrowForward size={14} />
+                    </div>
+                    <input type="checkbox" />
                 </div>
-                <div className="content">
-                    <Link to="/complete"> 제출 </Link>
+                <div className="submit-section">
+                    <button onClick={() => navigate('/complete')}>제출</button>
+
                 </div>
             </div>
         </>
