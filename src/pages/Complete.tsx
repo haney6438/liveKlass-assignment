0c46189823dacd8040af224aaf57b0fade9cc3b4
haney6438 <haney5438@gamil.com>
2026-05-23 15:35:51 +0900

    feat:결과 스텝 UI 구현

--- src/pages/Complete.tsx
+++ src/pages/Complete.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 
+import './Complete.css';
 import { MdOutlineCelebration, MdHorizontalRule } from "react-icons/md";
 
 function Complete() {
@@ -9,26 +10,26 @@ function Complete() {
                 <div className="sub-container">
                     <div className="notice-section">
                         <MdOutlineCelebration size={64} />
-                        <p>수강신청 되었습니다 </p>
-                        <p>DI502JDF3</p>
-                        <MdHorizontalRule size={64} />
+                        <p>신청되었습니다</p>
+                        <p style={{ fontSize: '14px' }}>DI502JDF3</p>
+                        <MdHorizontalRule size={32} />
                     </div>
                     <div className="check-section2">
                         <div className="indi-wrap">
-                            <p style={{ fontSize: '20px', fontWeight: 'bold' }}>신청자</p>
-                            <p style={{ fontSize: '14px' }}>이름: </p>
-                            <p style={{ fontSize: '14px' }}>이메일:</p>
-                            <p style={{ fontSize: '14px' }}>전화번호: </p>
-                            <p style={{ fontSize: '14px' }}>수강동기:</p>
+                            <p style={{ fontSize: '14px', fontWeight: 'bold' }}>신청자</p>
+                            <p style={{ fontSize: '10px' }}>이름: </p>
+                            <p style={{ fontSize: '10px' }}>이메일:</p>
+                            <p style={{ fontSize: '10px' }}>전화번호: </p>
+                            <p style={{ fontSize: '10px' }}>수강동기:</p>
                         </div>
                         <div className="group-wrap">
-                            <p style={{ fontSize: '20px', fontWeight: 'bold' }}>단체</p>
-                            <p style={{ fontSize: '14px' }}>단체명: </p>
-                            <p style={{ fontSize: '14px' }}>신청 인원:</p>
-                            <p style={{ fontSize: '14px' }}>참가자 명단: </p>
+                            <p style={{ fontSize: '14px', fontWeight: 'bold' }}>단체</p>
+                            <p style={{ fontSize: '10px' }}>단체명: </p>
+                            <p style={{ fontSize: '10px' }}>신청 인원:</p>
+                            <p style={{ fontSize: '10px' }}>참가자 명단: </p>
                         </div>
                     </div>
-                    <Link to="/">홈화면 </Link>
+                    <Link to="/">확인 </Link>
                 </div>
             </div>
         </>
